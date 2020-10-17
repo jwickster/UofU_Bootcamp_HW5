8c72e60284618041d18d539945aeb7ed2c030605
Jwickster <joshwickster@gmail.com>
2020-10-17 12:28:38 -0600

    add logic

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 $(document).ready(function() {
   
-
+$(".saveBtn").on("click", function() {
+  
+  //this gets obligatory values for text and time
+  console.log(this);
+  var text = $(this).siblings(".description").val();
+  var time = $(this).parent().attr("id");
+  
+  //store in local storage
+  localStorage.setItem(time, text)
+})
   
 })
